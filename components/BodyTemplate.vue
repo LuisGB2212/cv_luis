df6d05f87cae1e49eee2867337b058d53e68d4b0
Luisgb2212 <luis.gb2212@gmail.com>
2021-07-26 02:06:13 -0500

    Actualziaicón final

--- components/BodyTemplate.vue
+++ components/BodyTemplate.vue
@@ -9,6 +9,9 @@
         </b-col>
         <b-col class="mt-4">
             <h1 class="text-dark">Acerca de mi</h1>
+            <hr>
+            Soy una persona resolutiva, con gran capacidad para adaptarme a equipos y especializada en programación para web en formato escritorio y responsive, además de en el desarrollo de aplicaciones móviles.
+Me apasiona todo lo digital y en mis experiencias profesionales anteriores he cosechado varios logros que pueden consultar con las personas de referencia que he incluido en este currículum.
         </b-col>
       </b-row>
     </b-container>
